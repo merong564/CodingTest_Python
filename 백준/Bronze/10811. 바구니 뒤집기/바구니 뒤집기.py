5218c27eb16c77151b1aae5cdb7f00fda0937a60
merong564 <161675231+merong564@users.noreply.github.com>
2025-06-15 23:50:57 +0900

    Update 바구니 뒤집기.py
    답안 추가

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,27 +1,37 @@
-n, m = map(int, input().split())
-list = list(range(1, n+1))
-#print(list)
+# n, m = map(int, input().split())
+# list = list(range(1, n+1))
+# #print(list)
 
-for i in range(m):
-  a, b = map(int, input().split())
+# for i in range(m):
+#   a, b = map(int, input().split())
   
-  # index 헷갈리지 않도록 1씩 빼기
-  a -= 1
-  b -= 1
+#   # index 헷갈리지 않도록 1씩 빼기
+#   a -= 1
+#   b -= 1
   
-  for j in range(a,b+1):
+#   for j in range(a,b+1):
     
-    # 가운데 인덱스 이후부터는 값 바꾸기 중단    
-    if j >= (a+b)/2:
-      break
+#     # 가운데 인덱스 이후부터는 값 바꾸기 중단    
+#     if j >= (a+b)/2:
+#       break
       
-    # 합이 a+b가 되는 인덱스끼리 값을 바꿈
-    c = list[a+b-j]
-    d = list[j]
-    list[j] = c
-    list[a+b-j] = d
+#     # 합이 a+b가 되는 인덱스끼리 값을 바꿈
+#     c = list[a+b-j]
+#     d = list[j]
+#     list[j] = c
+#     list[a+b-j] = d
     
-    #print(list)
+#     #print(list)
 
-for i in list:
-  print(i, end=' ')
+# for i in list:
+#   print(i, end=' ')
+
+# 답안
+n, m = map(int, input().split())
+baskets = list(range(1, n+1))
+
+for _ in range(m):
+  i, j = map(int, input().split())
+  baskets[i-1:j] = reversed(baskets[i-1:j])
+
+print(*baskets)
