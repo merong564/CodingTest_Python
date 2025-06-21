d2574c086f3f38f35fb3e7c0e471e4675bdb7ece
merong564 <161675231+merong564@users.noreply.github.com>
2025-06-22 00:19:48 +0900

    Update 경로 찾기.py
    다른 사람 풀이 추가

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -33,3 +33,27 @@ for i in range(n):
   for j in range(n):
     print(visited[i][j], end=' ')
   print('')
+
+
+## 다른 사람 풀이
+import sys
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+result = [[0]*n for _ in range(n)]
+
+def dfs(start, cur_node, visited):
+  for next_node in range(n):
+    if not visited[next_node] and graph[cur_node][next_node]:
+      visited[next_node] = True
+      result[start][cur_node] = 1
+      dfs(start, next_node, visited)
+
+for i in range(n):
+  visited = [False]*n
+  dfs(i, i, visited)
+
+for row in result:
+  print(*row)
