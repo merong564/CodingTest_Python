d8495e2ec94c72aa4f11a5e1d6d8d226de61066f
merong564 <kmj564@sookmyung.ac.kr>
2025-06-23 01:49:10 +0900

    Create 2667_solution.py
    다른 풀이 추가

---
+++ 백준/Silver/2667. 단지번호 붙이기/2667_solution.py
@@ -0,0 +1,43 @@
+## DFS로 푸는 방법
+n = int(input())
+graph = []
+num = []
+
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+#print(graph)
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+def dfs(x,y):
+    if x<0 or x>=n or y<0 or y>=n:
+        return False
+    
+    if graph[x][y] == 1:
+        global cnt
+        cnt += 1
+        graph[x][y] = 0
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            dfs(nx, ny)
+        return True
+    return False
+
+cnt = 0
+result = 0
+
+for i in range(n):
+    for j in range(n):
+        if dfs(i,j) == True:
+            num.append(cnt)
+            result += 1
+            cnt = 0
+
+num.sort()
+print(result)
+for i in range(len(num)):
+    print(num[i])
