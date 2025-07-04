53d4a300e12f018cbd410711f82b69f901aafaf0
merong564 <161675231+merong564@users.noreply.github.com>
2025-07-04 11:47:45 +0900

    Update 음식물 피하기.py
    BFS 풀이 추가

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -1,7 +1,46 @@
+## DFS 풀이
+# import sys
+# input = sys.stdin.readline
+# sys.setrecursionlimit(10**5)
+
+# n, m, k = map(int, input().split())
+# matrix = [[0]*m for _ in range(n)]
+# for _ in range(k):
+#     r, c = map(int, input().split())
+#     matrix[r-1][c-1] = 1
+
+# dx = [1, -1, 0, 0]
+# dy = [0, 0, 1, -1]
+
+
+# def dfs(x, y):  
+#     global cnt
+#     if matrix[x][y] == 1:
+#         matrix[x][y] = 0
+#         for i in range(4):
+#             nx = x+dx[i]
+#             ny = y+dy[i]
+
+#             if (0<=nx<n) and (0<=ny<m):
+#                 if matrix[nx][ny] == 1:
+#                     cnt += 1
+#                     dfs(nx, ny)
+    
+# result = []
+
+# for i in range(n):
+#     for j in range(m):
+#         cnt = 0
+#         dfs(i, j)
+#         if cnt:
+#             result.append(cnt+1)
+# print(max(result))
+
+
+## BFS 풀이
 import sys
-# from collections import deque
+from collections import deque 
 input = sys.stdin.readline
-sys.setrecursionlimit(10**5)
 
 n, m, k = map(int, input().split())
 matrix = [[0]*m for _ in range(n)]
@@ -12,30 +51,30 @@ for _ in range(k):
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
+def bfs(x, y):
+    cnt = 0
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        x, y = queue.popleft()
+        if matrix[x][y] == 1:
+            cnt += 1
+            matrix[x][y] = 0
+            for i in range(4):
+                nx=x+dx[i]
+                ny=y+dy[i]
+                if (0<=nx<n) and (0<=ny<m):
+                    if matrix[nx][ny] == 1:
+                        queue.append((nx, ny))
+    return cnt
 
-def dfs(x, y):  
-    global cnt
-    if matrix[x][y] == 1:
-        matrix[x][y] = 0
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-
-            if (0<=nx<n) and (0<=ny<m):
-                if matrix[nx][ny] == 1:
-                    cnt += 1
-                    dfs(nx, ny)
-    
 result = []
 
 for i in range(n):
     for j in range(m):
-        cnt = 0
-        dfs(i, j)
+        cnt = bfs(i, j)
         if cnt:
-            result.append(cnt+1)
+            result.append(cnt)
+
 print(max(result))
-
-
-
 
