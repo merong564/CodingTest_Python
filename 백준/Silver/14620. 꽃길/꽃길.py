48074b055bc01c6f936b8398f4da71f74632ebf9
merong564 <161675231+merong564@users.noreply.github.com>
2025-07-19 14:19:59 +0900

    [Silver II] Title: 꽃길, Time: 228 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.py
+++ 백준/Silver/14620. 꽃길/꽃길.py
@@ -1,40 +1,49 @@
-N = int(input())
-G = [list(map(int, input().split())) for _ in range(N)]
+from itertools import combinations
 
-dx, dy = [0, 1, -1, 0, 0], [0, 0, 0, 1, -1]  # 방향벡터 (현재 위치와 상하좌우 4방향)
-anw = 10001  # 꽃 하나당 최고 비용 (200 * 5군데 = 1000), G의 범위가 0~200이라서 / 최대 비용인 1000을 넘는 임의의 수
+def check(map_list):
+    global answer
+    # visited로 꽃 잎이 겹치는지 확인
+    visited = []
+    total = 0
+    for x, y in map_list:
+        visited.append((x, y))
+        total += maps[x][y]
+        for i in range(4):
+            ax = x + dx[i]
+            ay = y + dy[i]
 
+            # 안 겹친다면 total에 더하기
+            if (ax, ay) not in visited:
+                total += maps[ax][ay]
+                visited.append((ax, ay))
 
-# 비용 계산
-def func(case: list) -> int:
-    anw = 0
-    flower = []  # 꽃의 위치 좌표를 쌓을 것임
-
-    # case는 꽃의 1차원 배열 좌표를 가져옴
-    for i in case:
-        # 1차원 배열 좌표를 2차원 배열 좌표로 변경 (화단 밖으로 나가는 경우를 제외시키기 위해)
-        x = i // N
-        y = i % N
-        if x == 0 or x == N - 1 or y == 0 or y == N - 1:
-            return 10001  # 최대 비용 넘는 것으로 산정해서 min 함수에 걸리게 함
-        # flower 리스트에 flower 위치 append / cost 계산
-        for d in range(5):  # d = direction
-            flower.append((x + dx[d], y + dy[d]))
-            anw += G[x + dx[d]][y + dy[d]]
-
-    # 꽃이 겹치는 경우 제외하기 위해 최대 비용 넘는 것으로 산정
-    if len(set(flower)) != 15:
-        return 10001
-
-    return anw
+            # 겹친다면 바로 끝 return
+            else:
+                return
+    # 최솟값을 구해야하기 때문에 담기
+    answer = min(answer, total)
 
 
-# main
-# 꽃의 위치를 2차원 배열에서 1차원 배열로 나타냄
-for i in range(N ** 2):
-    for j in range(i + 1, N ** 2):
-        for k in range(j + 1, N ** 2):
-            # 꽃의 위치가 1차원 배열로 나타나진 상태에서 func 함수로 비용을 구한 뒤에, min 함수로 최솟값을 구함
-            anw = min(anw, func([i, j, k]))
 
-print(anw)
+
+n = int(input())
+# 상하좌우
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+result = []
+maps = []
+answer = int(1e9)
+
+for _ in range(n):
+    maps.append(list(map(int, input().split())))
+
+# 꽃이 안 죽으려면 가장자리는 피해서 심어야 하기 때문에 안쪽 좌표만 result에 담기
+for i in range(1, n - 1):
+    for j in range(1, n - 1):
+        result.append((i, j))
+
+# combinations으로 씨앗을 심을 수 있는 좌표 check에 모두 담기
+for i in combinations(result, 3):
+    check(i)
+
+print(answer)
