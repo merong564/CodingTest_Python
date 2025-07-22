13f20a042e69c8806ac734114e79a4a8af4b1715
merong564 <161675231+merong564@users.noreply.github.com>
2025-07-23 00:20:21 +0900

    Update 폴리오미노.py
    다른 사람의 답안을 추가함.

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -1,10 +1,46 @@
+## 내 답안
+# import sys
+# sys.setrecursionlimit(10**5)
+
+
+# def find(board):
+#   global result
+#   if board == '':
+#     return
+    
+#   if board[:4] == 'XXXX':
+#     result += 'AAAA'
+#     new_board = board[4:]
+#     find(new_board)
+
+#   elif board[:2] == 'XX':
+#     result += 'BB'
+#     new_board = board[2:]
+#     find(new_board)
+
+#   elif board[0] == '.':
+#     result += '.'
+#     new_board = board[1:]
+#     find(new_board)
+
+#   else:
+#     print(-1)
+#     sys.exit()
+
+# board = str(input())
+# cnt = 0
+# result = ''
+# find(board)
+# print(result)
+
+## 다른 사람 답안 (겁나 간단함)
 board = input()
 
-board = board.replace("XXXX", "AAAA")
-board = board.replace("XX", "BB")
+board = board.replace('XXXX', 'AAAA')
+board = board.replace('XX', 'BB')
 
 if 'X' in board:
-    print(-1)
-    
+  print(-1)
+
 else:
-    print(board)
+  print(board)
