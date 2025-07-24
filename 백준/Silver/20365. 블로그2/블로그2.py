32c02a9a2e1b53fdc5b6c20f02ab1631e165e0d2
merong564 <161675231+merong564@users.noreply.github.com>
2025-07-25 00:41:18 +0900

    [Silver III] Title: 블로그2, Time: 152 ms, Memory: 33392 KB -BaekjoonHub

---
+++ 백준/Silver/20365. 블로그2/블로그2.py
@@ -0,0 +1,28 @@
+n = int(input())
+prob = input().strip()
+
+cnt_b = 0
+cnt_r = 0
+
+# B 카운팅
+before = 'R'
+for s in prob:
+  if s == 'B' and before == 'R':
+    cnt_b += 1
+  before = s
+
+# R 카운팅
+before = 'B'
+for s in prob:
+  if s == 'R' and before == 'B':
+    cnt_r += 1
+  before = s
+
+if cnt_b >= cnt_r:
+  result = cnt_r
+else:
+  result = cnt_b
+
+print(result+1)
+    
+  
