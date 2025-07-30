b3acadad79e07fc2c0450a1a8890bb27266d3cf7
merong564 <161675231+merong564@users.noreply.github.com>
2025-07-31 00:12:59 +0900

    [Silver I] Title: 민겸 수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/21314. 민겸 수/민겸 수.py
+++ 백준/Silver/21314. 민겸 수/민겸 수.py
@@ -1,35 +1,54 @@
-# pypy3
+num = input()
 
-import sys
-input = sys.stdin.readline
+def find_max(num):
+  cnt_m = 0
+  cnt = 0
+  result = ''
+  
+  for s in num:
+    cnt += 1
+    
+    # M 발견할 경우
+    if s == 'M':
+      # 마지막 글자일 경우
+      if cnt == len(num):
+        result += '1'*(cnt_m+1)
+      else:
+        cnt_m+=1
+  
+    # K 발견할 경우
+    if s == 'K':
+      if cnt_m == 0:  # K 앞에 M이 없을 경우 K 하나가 5가 됨
+        result += '5'
+      else:
+        result += str(5*10**cnt_m)
+        cnt_m = 0  # K 발견 시 M 카운트 초기화
+  
+  return result
 
-nums = list(input().rstrip())
+def find_min(num):
+  result = ''
+  cnt_m = 0
+  cnt = 0
+  
+  for s in num:
+    cnt += 1
+    
+    if s == 'M':
+      if cnt_m == 0:
+        result += '1'
+      if cnt == len(num):
+        result += '0'*(cnt_m)
+      cnt_m += 1
 
-def minn():
-    stack = []
-    res = ''
-    for num in nums:
-        if num == 'K':
-            if len(stack): res += str(10 ** (len(stack) - 1))
-            res += '5'
-            stack = []
-        else:
-            stack.append(num)
-    if len(stack): res += str(10 ** (len(stack) - 1))
-    return res
+    if s == 'K':
+      if cnt_m >= 2:
+        result += '0'*(cnt_m-1)
+      result += '5'
+      cnt_m = 0
 
-def maxx():
-    stack = []
-    res = ''
-    for num in nums:
-        if num == 'K':
-            res += str(5 * (10 ** len(stack)))
-            stack = []
-        else:
-            stack.append(num)
-    for _ in stack:
-        res += '1'
-    return res
-
-print(maxx())
-print(minn())
+  return result
+max = find_max(num)
+min = find_min(num)
+print(max)
+print(min)
