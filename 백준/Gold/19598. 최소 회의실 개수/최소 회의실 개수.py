79b6ee30cbffe32067668bf66017a36f30cc3a55
merong564 <161675231+merong564@users.noreply.github.com>
2025-08-06 23:36:29 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 440 ms, Memory: 48580 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
@@ -1,26 +1,34 @@
+from sys import stdin as s
+import heapq as hq
 
-from queue import PriorityQueue
- 
-N = int(input())
-courses = []
- 
-for _ in range(N) :
-    start, end = map(int, input().split())
-    courses.append((start, end))
- 
-courses.sort()
- 
-pq = PriorityQueue()
-pq.put(courses[0][1])
-count = 1
- 
-for i in range(1, N) :
-    earliest = pq.get()
-    if earliest <= courses[i][0] :
-        pq.put(courses[i][1])
-    else :
-        count += 1
-        pq.put(courses[i][1])
-        pq.put(earliest)
- 
-print(count)
+# s=open("input.txt","rt")
+
+N=int(s.readline().strip())
+
+
+meetings=[]             # N개의 회의
+rooms=[]                # 최대 N개의 회의실
+
+cnt=1                   # 회의실 1개 부터 시작
+
+hq.heappush(rooms,(0))  # 기존 회의실에 진행중인게 없음으로 0을 넣어둠
+
+for i in range(N):      # N개의 회의를 최소힙에 넣음
+    start,end=map(int,s.readline().split())
+    hq.heappush(meetings,(start,end))
+
+# N개의 회의를 하나씩 빼면서 진행중인 회의중에 
+# 가장 빨리 끝나는 회의를 찾아 교대 할지 새로 추가할지 판단.
+while meetings:        
+    start,end=hq.heappop(meetings)
+    earliest=hq.heappop(rooms)
+
+    if start>=earliest:
+        hq.heappush(rooms,end)
+    else:
+        cnt+=1
+        hq.heappush(rooms,earliest)
+        hq.heappush(rooms,end)
+
+
+print(cnt)
