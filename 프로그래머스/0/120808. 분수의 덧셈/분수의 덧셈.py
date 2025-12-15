88d205f202b660bbf66ce3db0918f933d43b15cf
merong564 <161675231+merong564@users.noreply.github.com>
2025-12-15 19:02:33 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,21 +1,38 @@
+# def solution(numer1, denom1, numer2, denom2):
+#     numer = numer1 * denom2 + numer2 * denom1
+#     denom = denom1 * denom2
+    
+#     # 풀이 1
+#     for i in range(min(numer, denom)+1, 1, -1):
+#         if numer % i == 0 and denom % i == 0:
+#             numer /= i
+#             denom /= i
+#     # 풀이 2
+#     # i = 2
+#     # while True:
+#     #     if i > max(numer, denom):
+#     #         break
+#     #     if numer % i == 0 and denom % i == 0:
+#     #         numer /= i
+#     #         denom /= i
+#     #     else:
+#     #         i += 1
+            
+#     answer = [numer, denom]
+#     return answer
+
+# 풀이 3
+import math
+
 def solution(numer1, denom1, numer2, denom2):
     numer = numer1 * denom2 + numer2 * denom1
     denom = denom1 * denom2
     
-    for i in range(min(numer, denom)+1, 1, -1):
-        if numer % i == 0 and denom % i == 0:
-            numer /= i
-            denom /= i
+    gcd = math.gcd(numer, denom)
+    numer /= gcd
+    denom /= gcd
     
-    # i = 2
-    # while True:
-    #     if i > max(numer, denom):
-    #         break
-    #     if numer % i == 0 and denom % i == 0:
-    #         numer /= i
-    #         denom /= i
-    #     else:
-    #         i += 1
-            
     answer = [numer, denom]
     return answer
+    
+    
