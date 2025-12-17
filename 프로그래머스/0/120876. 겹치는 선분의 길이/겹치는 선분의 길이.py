6a1f74e3c329c87033e6324ba663ef0ad06d8b4d
merong564 <161675231+merong564@users.noreply.github.com>
2025-12-17 22:16:25 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.05 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -26,3 +26,9 @@ def solution(lines):
             answer += 1
     
     return answer
+
+# 다른사람 풀이
+def solution(lines):
+    sets = [set(range(min(l), max(l))) for l in lines]
+    return len(sets[0] & sets[1] | sets[0] & sets[2] | sets[1] & sets[2])
+    
