495e13aac7fca3b8e4dd260a6da6aec49339d916
merong564 <161675231+merong564@users.noreply.github.com>
2025-12-19 00:29:46 +0900

    Update 연속된 수의 합.py

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,10 +1,10 @@
 def solution(num, total):
     answer = []
     sum = 0
-    for i in range(1, num+1):
+    for i in range(1, num):
         sum += i
         
-    first_num = (total - sum) / num + 1
+    first_num = (total - sum) / num
     
     for i in range(num):
         answer.append(first_num+i)
